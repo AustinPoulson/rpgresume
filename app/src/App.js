06e56eedcb51cd8568a7d14e1ff755ccebdf69a4
Austin Poulson <austinpoulson@gmail.com>
2023-09-26 16:27:42 -0500

    init react add background

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div style={style.mainContainer}>
+      
+    </div>
+  );
+}
+
+const style = {
+  mainContainer: {
+    //backgroundColor: 'red',
+    height: '100vh',
+  }
+};
